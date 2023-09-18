19ff71181baead20af1de819ae0852c30dcea363
Tacolinge <128391785+Tacolinge@users.noreply.github.com>
2023-09-18 18:56:55 +0200

    added taco exoskeleton

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,6 +1,4 @@
 local tacoport = table.deepcopy(data.raw.item["personal-roboport-mk2-equipment"])
-
-
 tacoport.name = "tacoport-mk3"
 tacoport.icons ={
     {
@@ -10,10 +8,19 @@ tacoport.icons ={
 }
 tacoport.placed_as_equipment_result = "tacoport-mk3"
 
+local tacoexo1 = table.deepcopy(data.raw.item["exoskeleton-equipment"])
+tacoexo1.name = "taco-exoskeleton-1"
+tacoexo1.icons = {
+    {
+        icon = tacoexo1.icon,
+        tint = {r=0,g=255,b=0,a=0.7}
+    },
+}
+
 --landfill stacksize
 local landfill = data.raw.item["landfill"]
 landfill.stack_size = 400
 
 
 
-data:extend({tacoport, landfill})
+data:extend({tacoport, tacoexo1, landfill})
--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -9,4 +9,18 @@ tacoport.ingredients = {
     {"personal-roboport-mk2-equipment", 10},
 }
 --tacoport.enabled = true --activate recipe from start
-data:extend{tacoport}
+
+local tacoexo1 = table.deepcopy(data.raw.recipe["personal-roboport-equipment"])
+tacoexo1.name = "taco-exoskeleton-1"
+tacoexo1.result = "taco-exoskeleton-1"
+tacoexo1.energy_required = 120
+tacoexo1.ingredients = {
+    {"exoskeleton-equipment", 3},
+    {"battery", 200},
+    {"se-holmium-solenoid", 200},
+    {"se-quantum-processor", 5},
+    {"personal-roboport-mk2-equipment", 10},
+}
+tacoexo1.enabled = true
+
+data:extend{tacoport, tacoexo1}
